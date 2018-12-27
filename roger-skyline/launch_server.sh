257a172e07e736e97d2f02caf43e518e08fbff45
Vincent MUCCHIELLI <vincent@Vincents-MacBook-Pro.local>
2018-12-27 17:21:08 +0100

    automatic deploy

--- roger-skyline/launch_server.sh
+++ roger-skyline/launch_server.sh
@@ -10,9 +10,10 @@ sudo ufw allow 'Nginx Full'
 echo "ufw rules modified"
 
 echo "Creating auto-signed ssl"
-sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt
+sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt subj "/C=FR/CN=localhost"
 echo "creating dh group - this may take a while"
-sudo openssl dhparam -out /etc/nginx/dhparam.pem 2048
+sudo openssl dhparam -out /etc/nginx/dhparam.pem 512
+sudo touch /etc/nginx/snippets/self-signed.conf
 sudo echo "ssl_certificate /etc/ssl/certs/nginx-selfsigned.crt;\n" >> /etc/nginx/snippets/self-signed.conf
 sudo echo "ssl_certificate /etc/ssl/private/nginx-selfsigned.key;\n" >> /etc/nginx/snippets/self-signed.conf
 sudo cp ssl_params.conf /etc/nginx/snippets/
@@ -27,6 +28,6 @@ sudo service nginx restart
 
 #Copie des fichiers web
 sudo mkdir /var/www/landing
-sudo cp site_vitrine/* /var/www/landing/
+sudo cp -r site_vitrine/* /var/www/landing/
 
 echo "Fin du deploiement!"
