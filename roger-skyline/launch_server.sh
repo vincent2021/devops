63e05c382463591ac62aa545e21f24b526752617
Vincent MUCCHIELLI <vincent@Vincents-MacBook-Pro.local>
2018-12-27 17:11:21 +0100

    automatic deploy

--- roger-skyline/launch_server.sh
+++ roger-skyline/launch_server.sh
@@ -12,7 +12,7 @@ echo "ufw rules modified"
 echo "Creating auto-signed ssl"
 sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt
 echo "creating dh group - this may take a while"
-sudo openssl dhparam -out /etc/nginx/dhparam.pem 4096
+sudo openssl dhparam -out /etc/nginx/dhparam.pem 2048
 sudo echo "ssl_certificate /etc/ssl/certs/nginx-selfsigned.crt;\n" >> /etc/nginx/snippets/self-signed.conf
 sudo echo "ssl_certificate /etc/ssl/private/nginx-selfsigned.key;\n" >> /etc/nginx/snippets/self-signed.conf
 sudo cp ssl_params.conf /etc/nginx/snippets/
