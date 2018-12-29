4021b81a771bd683365bd6a85915d3bc17295c11
Vincent MUCCHIELLI <vincent@Vincents-MacBook-Pro.local>
2018-12-29 13:21:55 +0100

    correcting script

--- roger-skyline/linux_config.sh
+++ roger-skyline/linux_config.sh
@@ -2,19 +2,19 @@
 echo  -e "\033[33mConfiguration of the UNIX OS (root required)\033[0m"
 echo -e "\033[31mPlease enter your personal username:\033[0m"
 read $PERSO
+read -p "Configuration for $PERSO, press enter"
 
-echo  -e "\033[33maptupdtae/upgrade & installation of required packages\033[0m"
+echo  -e "\033[33maAPT update/upgrade & installation of required packages\033[0m"
 apt-get update && apt-get upgrade -y
 echo  -e "\033[33mInstsallation des packages\033[0m"
 apt-get install -y curl zsh sudo ufw fail2ban portsentry mailutils
 
 echo  -e "\033[33mAdding your username to sudo group\033[0m"
 adduser $PERSO sudo
-
-echo  -e "\033[33mGetting oh_my_zsh & VIM upgrade\033[0m"
+read -p "Checking"
+echo  -e "\033[33mSwitching to ZSH & VIM upgrade\033[0m"
 chsh -s /bin/zsh
 chsh -s /bin/zsh $PERSO
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 apt-get upgrade -y vim
 echo -e "set nu\nsyntax on\nset mouse=a\n" > ~/.vimrc
 
@@ -53,3 +53,5 @@ crontask3="0 0 * * * sh /home/$PERSO/cron_scripts/cron_integrity.sh"
 echo  -e "\033[33mAutomatic configuration done\nPlease complete the SSH configuration (keys auth).\n
 On host: command ssh-copy-id $PERSO@hostname, then on Guest's SSH config set PasswordAuthentication no & restart ssh\n
 Network: setup a fixed IP with a subnet-netmask /30, see https://gist.github.com/pjdietz/5768124 \n Then reboot & stop undesired services !\n\033[0m"
+read -p "Press enter to install oh_my_zsh and end the script"
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
