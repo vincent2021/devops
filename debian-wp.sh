b129fd5667921ea0511d14643e6536d28001af40
Vincent MUCCHIELLI <vimucchi@e2r5p15.42.fr>
2019-03-07 12:48:18 +0100

    wp install script

--- debian-wp.sh
+++ debian-wp.sh
@@ -7,7 +7,7 @@ read -p "Configuration for $PERSO, press enter"
 echo  -e "\033[33mAPT update/upgrade & installation of required packages\033[0m"
 apt-get update -qq && apt-get upgrade -y -qq
 echo  -e "\033[33mInstsallation des packages\033[0m"
-apt-get install -y cron curl zsh sudo ufw fail2ban portsentryi mailutils
+apt-get install -y cron curl zsh sudo ufw fail2ban portsentry
 
 echo  -e "\033[33mAdding your username to sudo group\033[0m"
 adduser $PERSO sudo
@@ -16,7 +16,7 @@ echo  -e "\033[33mSwitching to ZSH & VIM upgrade\033[0m"
 chsh -s /bin/zsh
 chsh -s /bin/zsh $PERSO
 apt-get upgrade -y -qq vim
-echo -e "set nu\nsyntax on\nset mouse=a\ncolo torte\n" > ~/.vimrc
+echo -e "set nu\nsyntax on\nset mouse=a\ncolo torte\n" > ~/$PERSO/.vimrc
 
 echo  -e "\033[33mSSH configuration: no root login & port 2222\033[0m"
 read -p "Press enter to edit the config file"
@@ -46,17 +46,21 @@ chmod +x /home/$PERSO/cron_scripts/*
 
 crontask1="0 4 * * 1 sh /home/$PERSO/cron_scripts/cron_apt_maj.sh"
 (crontab -u root -l; echo "$crontask1" ) | crontab -u root -
-# Mise a jour au redemarrage
-# crontask2="@reboot sh /home/$PERSO/cron_scripts/cron_apt_maj.sh"
-# (crontab -u root -l; echo "$crontask2" ) | crontab -u root -
-# Alerte en cas de modification de crontab 
-# crontask3="0 0 * * * sh /home/$PERSO/cron_scripts/cron_integrity.sh"
-# (crontab -u root -l; echo "$crontask3" ) | crontab -u root -
 
 echo  -e "\033[33mAutomatic configuration done\n
-Network: setup a fixed IP with a subnet-netmask /30, see https://gist.github.com/pjdietz/5768124\n
-Please also complete the SSH configuration (keys auth).\n
-On host: command ssh-copy-id $PERSO@hostname, then on Guest's SSH config set PasswordAuthentication no & restart ssh\n
-Then reboot & stop undesired services !\n\033[0m"
+On host: command ssh-copy-id $PERSO@hostname, then on Guest's SSH config set PasswordAuthentication no & restart ssh\n\033[0m"
+
 read -p "Press enter to install oh_my_zsh and end the script"
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+read -p "Press enter to install Nginx"
+sudo apt-get install -y -qq nginx openssl
+echo  -e "\033[33mNginx installed\033[0m"
+
+#Autoriser Nginx sur notre firewall
+sudo ufw allow 'Nginx Full'
+echo  -e "\033[33mufw rules modified, don't forget to modify fail2ban\n\033[0m"
+
+read -p "Press enter to install OpenLiteSpeed and Wordpress"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/litespeedtech/ols1clk/master/ols1clk.sh)"
+echo  -e "\033[33mInstallation Complete. Please reboot!\n\033[0m"
