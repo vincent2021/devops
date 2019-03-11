3886c2c3d4860ad7a8b54456d39df9fbf1d1f2ea
vincent2021 <vincent2021@gmail.com>
2019-03-11 17:19:26 +0100

    dockerfile done

---
+++ docker/01_dockerfiles/ex02/hw/db/seeds.rb
@@ -0,0 +1,11 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+Country.create(name: 'Germany', population: 81831000)
+Country.create(name: 'France', population: 65447374)
+Country.create(name: 'Belgium', population: 10839905)
+Country.create(name: 'Netherlands', population: 16680000)
